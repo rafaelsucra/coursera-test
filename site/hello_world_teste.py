6b35e29d1b54fbc71ba192863a3e68d237eb96c8
Oliveira <rafael.e.oliveira@capgemini.com>
2020-03-23 21:39:00 -0300

    linguagem Python

---
+++ site/hello_world_teste.py
@@ -0,0 +1,22 @@
+print("hello world!!Porra!!")
+
+nome="Python é top"
+
+print(nome.upper())
+
+print(nome.lower())
+
+print('sem ', end="")
+print('quebra ', end="")
+print('de ', end="")
+print('linha ', end="")
+
+nome = input ("\nDigite o seu nome:")
+print("Olá "+ str(nome))
+
+a =int(input("\nDigite o primeiro num:"))
+b =int(input("Digite o segundo num:"))
+print("resultado: "+ str(a+b))
+
+a =float(input("Digite o um número:"))
+print("número digitado: "+ str (a))
