815d6668a0cc7fd10fa151fd28f788540c5d84ef
Oliveira <rafael.e.oliveira@capgemini.com>
2020-06-02 08:20:11 -0300

    Mobile App

---
+++ Projeto_Flutter/flutter_app_primeiro/android/app/src/main/java/rafaelsucra_flutterappprimeiro/flutter_app_primeiro/MainActivity.java
@@ -0,0 +1,6 @@
+package rafaelsucra_flutterappprimeiro.flutter_app_primeiro;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ Projeto_Flutter/primeiro_flutter_app/android/app/src/main/java/rafaelsucra/primeiroflutterapp/MainActivity.java
@@ -0,0 +1,6 @@
+package rafaelsucra.primeiroflutterapp;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
